7b85d3a28323816dff0c9b082f51749c84c44e02
max46q <2000maksym2000@gmail.com>
2024-06-02 17:21:13 +0300

    add asets and css

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-const start = () => {
+const start = async () => {
   try {
     app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
   } catch (err) {
