f70e3c6c637a7291c1baf0077f96c4ed68c756c5
max46q <2000maksym2000@gmail.com>
2024-06-02 17:33:13 +0300

    add conect to db

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const ejs = require("ejs");
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
+app.use(express.static(__dirname + "/views"));
+app.use(express.json());
+
+require("dotenv").config();
+
 const start = async () => {
   try {
+    await mongoose.connect(`${process.env.DB_URL}`);
     app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
   } catch (err) {
     console.log(err);
